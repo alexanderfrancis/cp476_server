1734d355d499b924d151d471f86c26870e1e6ec2
mklaso <mklasninovski@gmail.com>
2022-03-30 01:02:21 -0400

    finished checkout/cart page and fixed small css problems w/ buttons across pages.

--- cart.php
+++ cart.php
@@ -68,6 +68,114 @@
       </ul>
     </div>
 
-    <h1>placeholder</h1>
+   <!-- probably need to wrap everything under this in php when dynamically loading the products in the cart (based on ids? in the cart), set up variables for subtotal, tax, and total and add the cost of each item in the cart to subtotal and apply a tax of 13%? to the subtotal then just add the two together and display as total -->
+
+   <!-- also need to account for the quantity of each item, and based on a change in quantity -> reflect onto frontend -> just multiply cost by quantity, and make sure the stock of each item is reduced by the quantity accurately (might require php form to apply the change in quantity on checkout? not sure-->
+ 
+    <div class="checkout-container">
+      <h1>Summary</h1>
+      <div class="summary-container">
+        <p class="summary-text">Subtotal</p>
+        <p class="summary-cost">$0.00</p>
+      </div>
+      <div class="summary-container">
+        <p class="summary-text">Taxes</p>
+        <p class="summary-cost">$0.00</p>
+      </div>
+      <div class="summary-container">
+        <p class="summary-text">Total</p>
+        <p class="summary-cost">$0.00</p>
+      </div>
+      <button class="checkout-btn">Checkout</button>
+    </div>
+
+    <div class="cart-container">
+      <div class="cart-product">
+        <div class="cart-img-container">
+          <img
+            src="./images/casual/airforce1s.png"
+            alt="Product 1"
+            class="product-img"
+          />
+        </div>
+        <div class="cart-product-info-container">
+          <div class="cart-product-info">
+            <h2 class="cart-product-name">Air Force 1s</h2>
+            <p class="cart-product-cost">$135</p>
+          </div>
+          <p class="cart-product-brand">Nike</p>
+          <p class="cart-product-type">Casual</p>
+          <div class="cart-product-quantity-container">
+            <label for="quantity" class="cart-product-quantity">Quantity</label>
+            <select name="quantity" id="quantity">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+        <button class="product-remove-btn">Remove</button>
+      </div>
+
+      <div class="cart-product">
+        <div class="cart-img-container">
+          <img
+            src="./images/boots/dragon.png"
+            alt="Product 2"
+            class="product-img"
+          />
+        </div>
+        <div class="cart-product-info-container">
+          <div class="cart-product-info">
+            <h2 class="cart-product-name">The Dragon Boot 3.0</h2>
+            <p class="cart-product-cost">$240</p>
+          </div>
+          <p class="cart-product-brand">Taft</p>
+          <p class="cart-product-type">Boots</p>
+          <div class="cart-product-quantity-container">
+            <label for="quantity" class="cart-product-quantity">Quantity</label>
+            <select name="quantity" id="quantity">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+        <button class="product-remove-btn">Remove</button>
+      </div>
+
+      <div class="cart-product">
+        <div class="cart-img-container">
+          <img
+            src="./images/running/flyknits.png"
+            alt="Product 3"
+            class="product-img"
+          />
+        </div>
+        <div class="cart-product-info-container">
+          <div class="cart-product-info">
+            <h2 class="cart-product-name">React Infinity Run Flyknit 2</h2>
+            <p class="cart-product-cost">$210</p>
+          </div>
+          <p class="cart-product-brand">Nike</p>
+          <p class="cart-product-type">Running</p>
+          <div class="cart-product-quantity-container">
+            <label for="quantity" class="cart-product-quantity">Quantity</label>
+            <select name="quantity" id="quantity">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+        <button class="product-remove-btn">Remove</button>
+      </div>
+    </div>
   </body>
 </html>
